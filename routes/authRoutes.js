5631bb531bcd7a83a2f62b4f400b0e52684068ac
robdcampbell <rdeancampbell@gmail.com>
2022-02-15 16:45:41 -0500

    edit authController.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ import {
   deleteUser,
   updateUser,
   getUser,
-} from "../controllers/userController.js";
+} from "../controllers/authController.js";
 
 router.route("/").post(createUser).get(getAllUsers);
 router.route("/:uuid").get(getUser).patch(updateUser).delete(deleteUser);
